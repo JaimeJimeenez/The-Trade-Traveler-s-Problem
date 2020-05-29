2d0965c8c5063d5826d4a336c6ff53c4d0dcc8e1
Jaime Jiménez <55250735+Jimmyjimmenez@users.noreply.github.com>
2020-05-29 19:50:20 +0200

    Update Practice Case.cpp

--- Practice Case.cpp
+++ Practice Case.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 
+#include "Graph.h"
+#include "Set.h"
+#include "edgeGraph.h"
+
+std::vector <std::vector <int>> writeVertices() {
+	std::vector <std::vector <int>> returnVector(20, std::vector<int>(20, 0));
+	int imputCost;
+	for (int i{ 0 }; i < 20; i++) {
+		for (int j{ 0 }; j < 20; j++) {
+			std::cout << "Cost: ";
+			std::cin >> imputCost;
+			returnVector.at(i).at(j) = imputCost;
+		}
+	}
+	return returnVector;
+};
+
 int main() {
 
-	std::cout << "hello World.\n";
-	
+	std::vector <std::vector <int>> costsEdges = writeVertices();
+	Graph newGraph(20, costsEdges);
+
+
 	return 0;
 }
