18c3415c5d5f30e1709e704986185571fd7071fb
Jaime Jiménez <55250735+Jimmyjimmenez@users.noreply.github.com>
2020-05-29 19:50:00 +0200

    Update Graph.h

--- Graph.h
+++ Graph.h
@@ -12,6 +12,7 @@ class Graph
 public:
 
 	Graph(int vertices);
+	Graph(int vertices, std::vector<std::vector<int>> costs);
 	~Graph();
 
 	void insert(int v, int w, int cost);
@@ -20,7 +21,7 @@ public:
 
 	std::list<edgeGraph> travelingSalesmanNeighbour(int startVertex);
 	std::list<edgeGraph> travelingSalesmanPrim(int startVertex);
-	std::string printVector(const std::string& s);
+	std::string printVector(const std::string& s, std::vector <int> actualVector);
 	std::string print(const std::string& s);
 	std::string depth(int start);
 
@@ -31,10 +32,9 @@ private:
 	static const int INFINITE = std::numeric_limits<unsigned short int>::max();
 
 	int vertices;
-	std::vector<std::vector<int>> edges; //Quitado std::shared_ptr
-	int notVisitedVertex(const std::vector<std::shared_ptr<bool>>& visited , int vertices);
+	std::vector<std::vector<int>> edges; 
+	int notVisitedVertex(const std::vector<std::shared_ptr<bool>>& visited, int vertices);
 	std::string depth(int vertex, std::vector<std::shared_ptr<bool>>& visited);
 	edgeGraph MinimumEdgeCost(const Set& W, int v, int w);
 
 };
-
